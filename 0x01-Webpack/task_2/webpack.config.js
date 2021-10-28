a7929f55af9dbd494f9bbc95c906b74af31e31b9
Cort Robinson <cortrobinson99@gmail.com>
2021-10-28 17:17:38 -0500

    add task_2 dir and files

---
+++ 0x01-Webpack/task_2/webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+
+const config = {
+  entry: './js/dashboard_main.js',
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'bundle.js'
+  },
+  mode: "production",
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/,
+        type: 'asset/resource',
+      },
+    ]
+  }
+};
+
+module.exports = config;
