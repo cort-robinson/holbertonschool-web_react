7794abb85c74dd79d437e50db61eb64817975f7d
Cort Robinson <cortrobinson99@gmail.com>
2021-10-28 22:51:08 -0500

    add task_3 files

---
+++ 0x01-Webpack/task_3/webpack.config.js
@@ -0,0 +1,67 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const path = require('path');
+
+const config = {
+  entry: {
+    header: {
+      import: './modules/header/header.js',
+      dependOn: 'shared'
+    },
+    body: {
+      import: './modules/body/body.js',
+      dependOn: 'shared'
+    },
+    footer: {
+      import: './modules/footer/footer.js',
+      dependOn: 'shared'
+    },
+    shared: ['lodash', 'jquery']
+  },
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: '[name].bundle.js'
+  },
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        use: [
+          {
+            loader: 'img-optimize-loader'
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new CleanWebpackPlugin({
+      cleanOnceBeforeBuildPatterns: ['**/*']
+    }),
+    new HtmlWebpackPlugin({
+      title: 'Task 3',
+      favicon: './assets/favicon.ico'
+    })
+  ],
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        vendor: {
+          test: /[\\/]node_modules[\\/]/,
+          name: 'vendors',
+          chunks: 'all'
+        }
+      }
+    }
+  }
+};
+
+module.exports = config;
