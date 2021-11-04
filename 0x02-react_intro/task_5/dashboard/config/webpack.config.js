e0987c4ea1abcf78beb249c597c12049eeb09161
Cort Robinson <cortrobinson99@gmail.com>
2021-11-03 23:55:17 -0500

    add task 8 files

---
+++ 0x02-react_intro/task_5/dashboard/config/webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+
+const config = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: 'bundle.js',
+  },
+  devServer: {
+    contentBase: path.join(__dirname, './dist'),
+    open: true
+  },
+  devtool: 'inline-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(gif|png|jpe?g|svg)$/i,
+        use: [
+          'file-loader',
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              bypassOnDebug: true, // webpack@1.x
+              disable: true, // webpack@2.x and newer
+            },
+          },
+        ],
+      }
+    ],
+  },
+};
+
+module.exports = config;
