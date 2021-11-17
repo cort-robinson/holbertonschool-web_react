3ec046b2b679c523b6cfacd3ccb926df507421de
Cort Robinson <cortrobinson99@gmail.com>
2021-11-17 00:43:10 -0600

    add task_5 dir for task 5

---
+++ 0x03-react_props/task_5/dashboard/config/webpack.config.js
@@ -0,0 +1,58 @@
+const path = require('path');
+
+const config = {
+  entry: path.join(__dirname, '../src', 'index.js'),
+  output: {
+    path: path.resolve(__dirname, '../public'),
+    filename: 'bundle.js'
+  },
+  devServer: {
+    static: path.join(__dirname, '../dist'),
+    open: true,
+    hot: true
+  },
+  devtool: 'inline-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.(gif|png|jpe?g|svg)$/i,
+        use: [
+          'file-loader',
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              bypassOnDebug: true, // webpack@1.x
+              disable: true // webpack@2.x and newer
+            }
+          }
+        ]
+      },
+      {
+        test: /\.?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        }
+      },
+      {
+        test: /\.?jsx$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        }
+      }
+    ]
+  }
+};
+
+module.exports = config;
---
+++ 0x03-react_props/task_5/dashboard/src/App/App.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './App.css';
+import Notifications from '../Notifications/Notifications';
+import Header from '../Header/Header';
+import Login from '../Login/Login';
+import CourseList from '../CourseList/CourseList';
+import Footer from '../Footer/Footer';
+import { getLatestNotification } from '../utils/utils';
+
+function App({ isLoggedIn }) {
+  const listCourses = [
+    { id: 1, name: 'ES6', credit: 60 },
+    { id: 2, name: 'Webpack', credit: 20 },
+    { id: 3, name: 'React', credit: 40 },
+  ];
+
+  const htmlObject = {
+    __html: getLatestNotification(),
+  };
+
+  const listNotifications = [
+    { id: 1, type: 'default', value: 'New course available' },
+    { id: 2, type: 'urgent', value: 'New resume available' },
+    { id: 3, type: 'urgent', html: htmlObject },
+  ];
+  return (
+    <>
+      <Notifications listNotifications={listNotifications} />
+      <div class="App">
+        <Header />
+        <div class="App-body">
+          {isLoggedIn ? <CourseList listCourses={listCourses} /> : <Login />}
+        </div>
+        <div class="App-footer">
+          <Footer />
+        </div>
+      </div>
+    </>
+  );
+}
+
+App.propTypes = {
+  isLoggedIn: PropTypes.bool,
+};
+
+App.defaultProps = {
+  isLoggedIn: false,
+};
+
+export default App;
