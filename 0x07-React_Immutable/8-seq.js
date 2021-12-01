1962478c151844efaad6f663ecdb0bd7168afe74
Cort Robinson <cortrobinson99@gmail.com>
2021-11-30 21:43:25 -0600

    update task 8 file, add @babel/node dev-dep

--- 0x07-React_Immutable/8-seq.js
+++ 0x07-React_Immutable/8-seq.js
@@ -2,9 +2,13 @@ import { Seq } from 'immutable';
 
 function printBestStudents(object) {
   Seq(object)
-    .filter(student => student.score < 70)
-    .map(student => `${student.firstName} ${student.lastName}`)
-    .forEach(name => console.log(name));
+    .filter((student) => student.score >= 70)
+    .map((student) => ({
+      score: student.score,
+      firstName: student.firstName.charAt(0).toUpperCase() + student.firstName.slice(1),
+      lastName: student.lastName.charAt(0).toUpperCase() + student.lastName.slice(1),
+    }))
+    .forEach((name) => console.log(name));
 }
 
 export default printBestStudents;
