217a0a55746b8b628fc5dc1a86472c6a6a312013
Cort Robinson <cortrobinson99@gmail.com>
2021-11-30 21:17:19 -0600

    lint fix

--- 0x07-React_Immutable/4-mutations.js
+++ 0x07-React_Immutable/4-mutations.js
@@ -6,10 +6,10 @@ const map = Map({
   3: 'Elijah',
   4: 'Oliver',
   5: 'Jacob',
-  6: 'Lucas'
+  6: 'Lucas',
 });
 
-const map2 = List(map).withMutations(list => {
+const map2 = List(map).withMutations((list) => {
   list.set(2, 'Benjamin').set(4, 'Oliver');
 });
 
