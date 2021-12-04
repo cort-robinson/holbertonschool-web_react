0a1676f0d199cbe0c73ac1f6fcdaab7a4921c22c
Cort Robinson <cortrobinson99@gmail.com>
2021-12-04 08:12:37 -0600

    add task_0 dir for task 0

---
+++ 0x06-react_state/task_0/dashboard/src/Footer/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <div className="App-footer">
+      <p>Copyright 2020 - holberton School</p>
+    </div>
+  );
+}
+
+export default Footer;
