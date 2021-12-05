d1c79855e4a4196c36b55ed2e59bf79c7190edf7
Cort Robinson <cortrobinson99@gmail.com>
2021-12-04 20:56:31 -0600

    add task_1 dir for task 1

---
+++ 0x06-react_state/task_1/dashboard/src/Login/Login.js
@@ -0,0 +1,74 @@
+import { css, StyleSheet } from "aphrodite";
+import React from "react";
+
+const styles = StyleSheet.create({
+  login: {
+    '@media (min-width: 900px)': {
+      padding: '2.5em',
+    },
+  },
+  loginLabelInput: {
+    marginRight: '0.7em'
+  },
+  input: {
+    '@media (max-width: 900px)': {
+      display: 'block',
+    }
+  }
+});
+
+class Login extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleLoginSubmit = this.handleLoginSubmit.bind(this);
+    this.handleChangeEmail = this.handleChangeEmail.bind(this);
+    this.handleChangePassword = this.handleChangePassword.bind(this);
+    this.state = {
+      isLoggedIn: false,
+      email: '',
+      password: '',
+      enableSubmit: false,
+    };
+  }
+
+  handleLoginSubmit(event) {
+    event.preventDefault();
+    this.setState({ isLoggedIn: true });
+    console.log('handleLoginSubmit()');
+  }
+
+  handleChangeEmail(event) {
+    this.setState({ email: event.target.value }, () => {
+      if (this.state.email !== '' && this.state.password !== '')
+        this.setState({ enableSubmit: true });
+    });
+  }
+
+  handleChangePassword(event) {
+    this.setState({ password: event.target.value }, () => {
+      if (this.state.email !== '' && this.state.password !== '')
+        this.setState({ enableSubmit: true });
+    });
+  }
+
+  render() {
+    return (
+      <div className={css(styles.login)}>
+        <p>Login to access the full dashboard</p>
+        <form onSubmit={this.handleLoginSubmit}>
+          <span className={css(styles.input)}>
+            <label className={css(styles.loginLabelInput)} htmlFor="email">Email:</label>
+            <input className={css(styles.loginLabelInput)} type="email" name="email" value={this.state.email} onChange={this.handleChangeEmail} id="email" />
+          </span>
+          <span className={css(styles.input)}>
+            <label className={css(styles.loginLabelInput)} htmlFor="pwd">Password:</label>
+            <input className={css(styles.loginLabelInput)} type="password" name="pwd" value={this.state.password} onChange={this.handleChangePassword} id="pwd" />
+          </span>
+          <input type="submit" disabled={!this.state.enableSubmit} />
+        </form>
+      </div>
+    );
+  }
+}
+
+export default Login;
---
+++ 0x06-react_state/task_1/dashboard/src/Notifications/NotificationItem.test.js
@@ -0,0 +1,39 @@
+import { StyleSheetTestUtils } from 'aphrodite';
+import { mount, shallow } from "enzyme";
+import React from "react";
+import NotificationItem from "./NotificationItem";
+import Notifications from "./Notifications";
+
+StyleSheetTestUtils.suppressStyleInjection();
+
+describe('NotificationItem', () => {
+  it('renders a NotificationItem component', () => {
+    const wrapper = shallow(<NotificationItem />);
+    expect(wrapper).toHaveLength(1);
+  });
+
+  it('renders component with type and value properties', () => {
+    const wrapper = shallow(<NotificationItem type="default" value="test" />);
+    expect(wrapper.props()['data-notification-type']).toEqual('default');
+    expect(wrapper.text()).toEqual('test');
+    expect(wrapper.html()).toContain('data-notification-type="default"');
+    expect(wrapper.html()).toContain('test</li>');
+  });
+
+  it('calls spy markAsRead property with correct id when component is clicked', () => {
+    const listNotifications = [
+      { id: 1, type: "default", value: "test1" },
+      { id: 2, type: "urgent", value: "test2" },
+      { id: 3, type: "default", value: "test3" },
+    ];
+    const wrapper = mount(<Notifications displayDrawer={true} listNotifications={listNotifications} />);
+    const instance = wrapper.instance();
+    const spy = jest.spyOn(instance, 'markAsRead');
+    wrapper.find('li').first().simulate('click');
+    expect(spy).toHaveBeenCalledWith(1);
+    wrapper.find('li').at(1).simulate('click');
+    expect(spy).toHaveBeenCalledWith(2);
+    wrapper.find('li').last().simulate('click');
+    expect(spy).toHaveBeenCalledWith(3);
+  });
+});
