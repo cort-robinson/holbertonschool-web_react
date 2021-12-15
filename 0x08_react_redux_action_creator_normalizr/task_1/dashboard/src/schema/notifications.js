217865df7feb18c1345d6bdeab9e8b8a625500a1
cort-robinson <cortrobinson99@gmail.com>
2021-12-14 20:25:53 -0600

    add task_1 dir for task 1

---
+++ 0x08_react_redux_action_creator_normalizr/task_1/dashboard/src/schema/notifications.js
@@ -0,0 +1,20 @@
+import { normalize, schema } from 'normalizr';
+import notifications from '../../../../notifications.json';
+
+const user = new schema.Entity('users');
+const message = new schema.Entity('messages', {}, { idAttribute: 'guid' });
+const notification = new schema.Entity('notifications', {
+  author: user,
+  context: message,
+});
+
+const mySchema = [notification];
+const normalizedNotifications = normalize(notifications, mySchema);
+
+function getAllNotificationsByUser(userId) {
+  return notifications
+    .filter((notification) => notification.author.id === userId)
+    .map((notification) => notification.context);
+}
+
+export { normalizedNotifications, getAllNotificationsByUser };
